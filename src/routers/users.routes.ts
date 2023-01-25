7053b54fdf2600747037e214254c5b3244c1661e
Antonio Prado <antonioeprado18@gmail.com>
2023-01-25 18:37:00 -0300

    feat: payload validation

--- src/routers/users.routes.ts
+++ src/routers/users.routes.ts
@@ -1,9 +1,11 @@
 import { signIn, signUp } from "@controllers/users.controllers";
+import { validateBody } from "@middlewares";
+import { signInSchema, signUpSchema } from "@schemas";
 import { Router } from "express";
 
 const usersRouter = Router();
 
-usersRouter.post("/sign-up", signUp);
-usersRouter.post("/sign-in", signIn);
+usersRouter.post("/sign-up", validateBody(signUpSchema), signUp);
+usersRouter.post("/sign-in", validateBody(signInSchema), signIn);
 
 export { usersRouter };
