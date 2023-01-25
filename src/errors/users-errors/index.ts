84e818350550c636b79c03eaf28321e82be3a9ac
Antonio Prado <antonioeprado18@gmail.com>
2023-01-25 18:34:52 -0300

    feat: invalid payload error

--- src/errors/users-errors/index.ts
+++ src/errors/users-errors/index.ts
@@ -1,4 +1,4 @@
-import { ApplicationError } from "@protocols";
+import { ApplicationError, InvalidPayloadError } from "@protocols";
 
 export function userNotRegistered(): ApplicationError {
   return {
@@ -11,5 +11,13 @@ export function wrongInfo(): ApplicationError {
   return {
     name: "wrongInfo",
     message: "Wrong email or password.",
+  };
+}
+
+export function invalidPayload(details: string[]): InvalidPayloadError {
+  return {
+    name: "InvalidPayloadError",
+    message: "Invalid payload",
+    details,
   };
 }
