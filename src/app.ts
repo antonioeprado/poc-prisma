f6c4b13a9894b03a52b27cc93c982aed07f65b31
Antonio Prado <antonioeprado18@gmail.com>
2023-01-27 16:33:54 -0300

    feat: all routes implemented

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import cors from "cors";
 import { loadEnv } from "@config";
+import {
+  authRouter,
+  familiesRouter,
+  productsRouter,
+  usersRouter,
+} from "@routers";
 
 loadEnv();
 
@@ -8,6 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.get("/health", (req, res) => res.send("Ok!"));
+app.use("/users", usersRouter);
+app.use("/auth/*", authRouter);
+app.use("/auth/products", productsRouter);
+app.use("/auth/families", familiesRouter);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`App running on port: ${PORT}`));
